bb5ea04717cf63b28e707dbd716d5913faa8dfa9
ArturAku12 <artur.akulenko@gmail.com>
2022-09-06 14:30:02 +0300

    progress bar

--- src/extra/route-element.tsx
+++ src/extra/route-element.tsx
@@ -21,13 +21,26 @@ interface RouteElementProps {
             {routeParts.map((part, ind) => {
                 const { text, hint, done, onClick } = part;
                 const isLastDone = ind === lastDone;
+                console.log(routeParts)
                 return (
-                    <div key={`${ind}`}
-                         className={clsx(done && 'routePartDone', isLastDone && 'isLastDone')}
-                         style={onClick ? {cursor: 'pointer'} : undefined}
-                         title={hint}
-                         onClick={onClick} >
-                        {text}
+                    <div title = {ind.toString()} className = {clsx((ind == 0 && done && routeParts[1].done == true) && "firstDoneContinue", 
+                                                           (ind == 0 && done &&routeParts[1].done == false) && "firstDoneStop",
+                                                           (ind == (routeParts.length - 1) && done) && "doneLast",
+                                                           (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == true) && "doneContinue",
+                                                           (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == false) && "doneStop",
+                                                           (ind !== 0 && !done &&ind !== (routeParts.length - 1)) && "notDoneContinue",
+                                                           (ind == (routeParts.length - 1) && !done) && "notDoneStop"
+                                                           )}
+                                            key = {ind}>
+                        <div key={`${ind}`}
+                            className={clsx(done && 'routePartDone', isLastDone && 'isLastDone', !done && "notDone", 
+                                            (ind == (routeParts.length - 1)) && "last",
+                                            (ind == 0) && "first")}
+                            style={onClick ? {cursor: 'pointer'} : undefined}
+                            title={hint}
+                            onClick={onClick} >
+                            {text}
+                        </div>
                     </div>
                 );
             })}
