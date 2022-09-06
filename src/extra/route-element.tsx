25f8ae21f0866d28f31bdf704bc4c7f989c9708f
ArturAku12 <artur.akulenko@gmail.com>
2022-09-06 17:53:24 +0300

    simplified logic

--- src/extra/route-element.tsx
+++ src/extra/route-element.tsx
@@ -23,19 +23,28 @@ interface RouteElementProps {
                 const isLastDone = ind === lastDone;
                 console.log(routeParts)
                 return (
-                    <div title = {ind.toString()} className = {clsx((ind == 0 && done && routeParts[1].done == true) && "firstDoneContinue", 
-                                                           (ind == 0 && done &&routeParts[1].done == false) && "firstDoneStop",
-                                                           (ind == (routeParts.length - 1) && done) && "doneLast",
-                                                           (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == true) && "doneContinue",
-                                                           (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == false) && "doneStop",
-                                                           (ind !== 0 && !done &&ind !== (routeParts.length - 1)) && "notDoneContinue",
-                                                           (ind == (routeParts.length - 1) && !done) && "notDoneStop"
+                    <div title = {ind.toString()} className = {clsx(
+                                                           (routeParts[ind + 1]?.done == true) && "continue",
+                                                           (routeParts[ind + 1]?.done == false || ind == (routeParts.length - 1)) && "stop",
+                                                           ind == 0 && "start",
+                                                           ind == (routeParts.length - 1) && "end"
+                                                        //    (ind == 0 && done && routeParts[1].done == true) && "firstDoneContinue", 
+                                                        //    (ind == 0 && done &&routeParts[1].done == false) && "firstDoneStop",
+                                                        //    (ind == (routeParts.length - 1) && done) && "doneLast",
+                                                        //    (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == true) && "doneContinue",
+                                                        //    (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == false) && "doneStop",
+                                                        //    (ind !== 0 && !done &&ind !== (routeParts.length - 1)) && "notDoneContinue",
+                                                        //    (ind == (routeParts.length - 1) && !done) && "notDoneStop",
+                                                           
                                                            )}
                                             key = {ind}>
                         <div key={`${ind}`}
-                            className={clsx(done && 'routePartDone', isLastDone && 'isLastDone', !done && "notDone", 
-                                            (ind == (routeParts.length - 1)) && "last",
-                                            (ind == 0) && "first")}
+                            className={clsx(
+                                            done && 'routePartDone',
+                                            !done && "notDone", 
+                                            (ind == 0) && "first",
+                                            (ind == (routeParts.length - 1)) && "last"
+                                            )}
                             style={onClick ? {cursor: 'pointer'} : undefined}
                             title={hint}
                             onClick={onClick} >
