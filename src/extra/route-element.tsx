bb170c5de5a51ad906517f70a993f4e6acac59c3
ArturAku12 <artur.akulenko@gmail.com>
2022-09-08 10:42:29 +0300

    remove unnecessary div layer

--- src/extra/route-element.tsx
+++ src/extra/route-element.tsx
@@ -23,34 +23,35 @@ interface RouteElementProps {
                 const isLastDone = ind === lastDone;
                 console.log(routeParts)
                 return (
-                    <div title = {ind.toString()} className = {clsx(
-                                                           (routeParts[ind + 1]?.done == true) && "continue",
-                                                           (routeParts[ind + 1]?.done == false || ind == (routeParts.length - 1)) && "stop",
-                                                           ind == 0 && "start",
-                                                           ind == (routeParts.length - 1) && "end"
-                                                        //    (ind == 0 && done && routeParts[1].done == true) && "firstDoneContinue", 
-                                                        //    (ind == 0 && done &&routeParts[1].done == false) && "firstDoneStop",
-                                                        //    (ind == (routeParts.length - 1) && done) && "doneLast",
-                                                        //    (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == true) && "doneContinue",
-                                                        //    (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == false) && "doneStop",
-                                                        //    (ind !== 0 && !done &&ind !== (routeParts.length - 1)) && "notDoneContinue",
-                                                        //    (ind == (routeParts.length - 1) && !done) && "notDoneStop",
+                    // <div title = {ind.toString()} className = {clsx(
+                    //                                        (routeParts[ind + 1]?.done == true) && "continue",
+                    //                                        (routeParts[ind + 1]?.done == false || ind == (routeParts.length - 1)) && "stop",
+                    //                                        ind == 0 && "start",
+                    //                                        ind == (routeParts.length - 1) && "end"
+                    //                                     //    (ind == 0 && done && routeParts[1].done == true) && "firstDoneContinue", 
+                    //                                     //    (ind == 0 && done &&routeParts[1].done == false) && "firstDoneStop",
+                    //                                     //    (ind == (routeParts.length - 1) && done) && "doneLast",
+                    //                                     //    (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == true) && "doneContinue",
+                    //                                     //    (ind !== 0 && ind !== (routeParts.length - 1) && done && routeParts[ind + 1].done == false) && "doneStop",
+                    //                                     //    (ind !== 0 && !done &&ind !== (routeParts.length - 1)) && "notDoneContinue",
+                    //                                     //    (ind == (routeParts.length - 1) && !done) && "notDoneStop",
                                                            
-                                                           )}
-                                            key = {ind}>
+                    //                                        )}
+                    //                         key = {ind}>
                         <div key={`${ind}`}
                             className={clsx(
                                             done && 'routePartDone',
-                                            !done && "notDone", 
+                                            !done && "notDone",
+                                            (routeParts[ind + 1]?.done == done) && "continue",
+                                            (routeParts[ind + 1]?.done != done) && "stop",
                                             (ind == 0) && "first",
-                                            (ind == (routeParts.length - 1)) && "last"
                                             )}
                             style={onClick ? {cursor: 'pointer'} : undefined}
                             title={hint}
                             onClick={onClick} >
                             {text}
                         </div>
-                    </div>
+                    //</div>
                 );
             })}
         </div>
