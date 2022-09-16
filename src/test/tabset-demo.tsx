3acdf2d48db8629792b8004722d55e50a726e1fb
ArturAku12 <artur.akulenko@gmail.com>
2022-09-16 11:23:07 +0300

    style update + setFinalState

--- src/test/tabset-demo.tsx
+++ src/test/tabset-demo.tsx
@@ -5,22 +5,18 @@ import TabSet from './tabsetter';
 import { createSyncProviders } from "../main/vdom-hooks";
 
 const App = () => {
-
-    const identity = {parent:"test"}
+    const ComponentDemo = (name:string) => {
+        return <div> Hello {name}</div>
+    }
+    const rightContent = [ComponentDemo("James"), ComponentDemo("Anny"), ComponentDemo("Robert"), ComponentDemo("Praetor")]
     const tabs = ["hellos", "hi", "bye", "so"]
     const state = {active: ""}
-    const child1 = $(TabSet, {tabs: tabs, state: state, identity: {parent: "test"}})
+    const child1 = $(TabSet, {tabs: tabs, state: state, rightContent: rightContent, identity: {parent: "test"}})
     const sender = {
         enqueue: (identity: any, patch: any) => console.log()
     };
     const ack: boolean | null = null;
     return createSyncProviders({sender, ack, children: child1})
-    return (
-        <div>
-            <TabSet tabs = {tabs} state = {state} identity = {{parent:"test"}}/>
-            <img src="https://i.pinimg.com/736x/46/d4/58/46d45821deaa745f44226aaf72c1031e--pictures-for-painting-silence-in-the-library.jpg"/>
-        </div>
-    )
 }
 
 const containerElement = document.createElement("div")
--- src/test/tabsetter.tsx
+++ src/test/tabsetter.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { createElement as $ } from "react";
 import { createSyncProviders } from "../main/vdom-hooks";
 import { Patch, PatchHeaders, useInputSync } from '../extra/input-sync';
-import "./popup-demo.scss"
+//import "./popup-demo.scss"
 
 
 
@@ -13,22 +13,25 @@ interface TabSetProps {
     state: {
         active: string
     },
+    rightContent?: ReactNode[]
 }
 
-const TabSet = ({tabs, state, identity}: TabSetProps) => {
-    
+const TabSet = ({tabs, state, identity, rightContent}: TabSetProps) => {
     const {
 		currentState, 
 		setTempState, 
 		setFinalState 
 	} = useInputSync(identity, 'receiver', state, false, patchToState, s => s, stateToPatch);
-    
-    console.log(currentState)
 
     function stateToPatch({ active }: any): Patch {
+
         return { value: active };
     }
     
+    const { active } = currentState
+
+    console.log(tabs.indexOf(active))
+
     function patchToState(patch: Patch): any {
         return {
             active: patch.value,
@@ -36,22 +39,30 @@ const TabSet = ({tabs, state, identity}: TabSetProps) => {
     }
 
     return (
-        <div style = {{display: 'flex'}}>
-            {/* <div> */}
+        <div>
+            <div>
             {tabs.map((tabName: string, key: number) => 
                 //<div className="clickableDiv">
                 <button 
-                className="clickableDiv"
+                className="tabsetDiv"
                 key = {key}
-                //style = {{backgroundColor: "blue"}}
-                onClick = {() => setTempState({active: tabName})}
-                style = {tabName == currentState.active ? { backgroundColor: "green"} : { backgroundColor: "white"}}
+                onClick = {() => setFinalState({active: tabName})}
+                style = {tabName == currentState.active ? { backgroundColor: "#009688"} : { backgroundColor: "white"}}
                 value = {tabName}
                 > {tabName} </button>
                 //</div>
             )}
-            {/* </div> */}
+            </div>
+            <div style={{display: "flex"}}>
+                <div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Fartygsmodell-AMPHION._1902-04_-_Sj%C3%B6historiska_museet_-_O_00031.tif/lossy-page1-300px-Fartygsmodell-AMPHION._1902-04_-_Sj%C3%B6historiska_museet_-_O_00031.tif.jpg"></img>
+                </div>
+                <div>
+                    {currentState.active !== "" ? rightContent![tabs.indexOf(active)] : null }
+                </div>
+            </div>
         </div>
+
     )
 }
 
