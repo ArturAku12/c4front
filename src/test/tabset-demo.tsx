51870bb77bed667afd0beb71ac8ce4d2903285d9
ArturAku12 <artur.akulenko@gmail.com>
2022-09-20 13:12:01 +0300

    contain alignment

--- src/test/tabset-demo.tsx
+++ src/test/tabset-demo.tsx
@@ -6,17 +6,18 @@ import { createSyncProviders } from "../main/vdom-hooks";
 
 const App = () => {
     const ComponentDemo = (name:string) => {
-        return <div> Hello {name}</div>
+        return <div> Hello {name} </div>
     }
     const rightContent = [ComponentDemo("James"), ComponentDemo("Anny"), ComponentDemo("Robert"), ComponentDemo("Praetor")]
     const tabs = ["hellos", "hi", "bye", "so"]
     const state = {active: ""}
     const child1 = $(TabSet, {tabs: tabs, state: state, rightContent: rightContent, identity: {parent: "test"}})
+    const child2 = $("img", {src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Fartygsmodell-AMPHION._1902-04_-_Sj%C3%B6historiska_museet_-_O_00031.tif/lossy-page1-300px-Fartygsmodell-AMPHION._1902-04_-_Sj%C3%B6historiska_museet_-_O_00031.tif.jpg", style: {marginTop: "0px, "}})
     const sender = {
         enqueue: (identity: any, patch: any) => console.log()
     };
     const ack: boolean | null = null;
-    return createSyncProviders({sender, ack, children: child1})
+    return createSyncProviders({sender, ack, children: [child1, child2]})
 }
 
 const containerElement = document.createElement("div")
--- src/test/tabsetter.tsx
+++ src/test/tabsetter.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useRef } from 'react';
 import { createElement as $ } from "react";
 import { createSyncProviders } from "../main/vdom-hooks";
 import { Patch, PatchHeaders, useInputSync } from '../extra/input-sync';
@@ -30,8 +30,6 @@ const TabSet = ({tabs, state, identity, rightContent}: TabSetProps) => {
     
     const { active } = currentState
 
-    console.log(tabs.indexOf(active))
-
     function patchToState(patch: Patch): any {
         return {
             active: patch.value,
@@ -39,24 +37,21 @@ const TabSet = ({tabs, state, identity, rightContent}: TabSetProps) => {
     }
 
     return (
-        <div>
-            <div>
+        <div id="tabSetter" style = {{display: "flex", justifyContent: "space-between"}}>
+            <div style={{display: 'flex',}}>
             {tabs.map((tabName: string, key: number) => 
                 //<div className="clickableDiv">
                 <button 
                 className="tabsetDiv"
                 key = {key}
-                onClick = {() => setFinalState({active: tabName})}
+                onClick = {() => {setFinalState({active: tabName})}}
                 style = {tabName == currentState.active ? { backgroundColor: "#009688"} : { backgroundColor: "white"}}
                 value = {tabName}
                 > {tabName} </button>
                 //</div>
             )}
             </div>
-            <div style={{display: "flex"}}>
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Fartygsmodell-AMPHION._1902-04_-_Sj%C3%B6historiska_museet_-_O_00031.tif/lossy-page1-300px-Fartygsmodell-AMPHION._1902-04_-_Sj%C3%B6historiska_museet_-_O_00031.tif.jpg"></img>
-                </div>
+            <div className = "tabsetDiv" style={{display: "flex", flexDirection: "row-reverse", }}>
                 <div>
                     {currentState.active !== "" ? rightContent![tabs.indexOf(active)] : null }
                 </div>
